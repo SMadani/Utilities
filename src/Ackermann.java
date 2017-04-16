3c3a3369dd0d3a59357a2661f283b107d4403289
Sina <sm1748@york.ac.uk>
2017-04-16 02:37:53 +0100

    Improved time formatting

--- src/Ackermann.java
+++ src/Ackermann.java
@@ -1,3 +1,7 @@
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 class Ackermann {
     static int ack(final int m, final int n)
     {
@@ -23,6 +27,17 @@ class Ackermann {
         final long endTime, startTime = System.nanoTime();
         final int result = ack(m, n);
         endTime = System.nanoTime();
-        System.out.printf("\nAckermann result = %d\nTime taken = %d ms", result, (endTime-startTime)/1000000);
+        System.out.println("ackermann("+m+", "+n+") = "+result);
+        System.out.println("Time taken = "+formatDuration(Duration.ofNanos(endTime-startTime)));
+    }
+
+    static String formatDuration(Duration duration) {
+    	String pattern = "";
+    	if (duration.toHours() >= 1)
+    		pattern += "H:";
+    	if (duration.toMinutes() >= 1)
+    		pattern += "m:";
+        pattern += "s.SSS";
+        return LocalTime.MIDNIGHT.plus(duration).format(DateTimeFormatter.ofPattern(pattern));
     }
 }
